881616c5d601b73485ef99599bf7b499c88e940a
ShoLee01 <miguelalfaro2017@gmail.com>
2022-06-08 15:29:21 -0500

    fix: solved to comment data

--- TopWay.API/TopWay/Resources/CommentResource.cs
+++ TopWay.API/TopWay/Resources/CommentResource.cs
@@ -5,5 +5,6 @@ public class CommentResource
     public int Id { get; set; }
     public string Description { get; set; }
     public double Score { get; set; }
+    public int ScalerId { get; set; }
     public DateTime Date { get; set; }
 }
